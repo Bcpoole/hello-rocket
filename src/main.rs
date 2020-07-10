488645021a441136fcb7c753ddb51ed3dd2d9d54
Brandon Poole <brandoncpoole@gmail.com>
2020-07-10 16:50:17 -0700

    Added dynamic hello path and serving of static files.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,23 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket_contrib::serve::StaticFiles;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+    format!("Hello, {}!", name.as_str())
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, hello])
+        .mount("/public", StaticFiles::from("static"))
+        .launch();
 }
