66ad41a971e9c1658e4e94e719410649eda1db75
Brandon Poole <brandoncpoole@gmail.com>
2020-07-10 17:17:59 -0700

    Forwarding ranked example. Query strings, optional parameters, adnd multiple segments with query guards.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@
 extern crate rocket;
 extern crate rocket_contrib;
 
+use rocket::http::RawStr;
 use rocket_contrib::serve::StaticFiles;
+
+use rocket::request::Form;
+
+#[derive(FromForm)]
+struct User {
+    name: String,
+    account: usize,
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -13,12 +22,43 @@ fn index() -> &'static str {
 
 #[get("/hello/<name>")]
 fn hello(name: String) -> String {
-    format!("Hello, {}!", name.as_str())
+    format!("Hello, {}!", name)
+}
+
+#[get("/hello?wave&<name>")]
+fn hello_wave(name: Option<String>) -> String {
+    name.map(|name| format!("Hi, {}!", name))
+        .unwrap_or_else(|| "Hello!".into())
+}
+
+#[get("/user/<id>")]
+fn user(id: usize) -> String {
+    format!("Hello (1) user {}!", id)
+}
+
+#[get("/user/<id>", rank = 2)]
+fn user_int(id: isize) -> String {
+    format!("Hello (2) user {}!", id)
+}
+
+#[get("/user/<id>", rank = 3)]
+fn user_str(id: &RawStr) -> String {
+    format!("Hello (3) user {}!", id.as_str())
+}
+
+//example http://localhost:8000/item?id=123&name=Bob&account=307
+#[get("/item?<id>&<user..>")]
+fn item(id: usize, user: Option<Form<User>>) -> String {
+    user.map(|u| format!("item {} for user {} aka {}!", id, u.name, u.account))
+        .unwrap_or_else(|| format!("item {}", id))
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, hello])
+        .mount(
+            "/",
+            routes![index, hello, hello_wave, user, user_int, user_str, item],
+        )
         .mount("/public", StaticFiles::from("static"))
         .launch();
 }
